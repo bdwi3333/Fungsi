8d0b8856c9b70714753c8126cc5471c2824720a7
bdwi3333 <128878186+bdwi3333@users.noreply.github.com>
2023-10-25 07:47:44 +0700

    Update kegiatan3.py

--- Modul 3/kegiatan3.py
+++ Modul 3/kegiatan3.py
@@ -1,12 +1,10 @@
-# Data list seperti pada kegiatan 1 modul 1
 random_list = [3.1, 2.7, 5.5, 19, 753, 412, 'Hello', 'Python', 'world', 'AI']
 
-# Filter untuk memisahkan nilai float, int, dan string
 data_float = list(filter(lambda x: isinstance(x, float), random_list))
 data_int = list(filter(lambda x: isinstance(x, int), random_list))
 data_string = list(filter(lambda x: isinstance(x, str), random_list))
 
-# Map untuk memetakan nilai int menjadi satuan, puluhan, dan ratusan
+
 def map_to_digits(num):
     return {
         'ratusan': num // 100,
@@ -16,7 +14,7 @@ def map_to_digits(num):
 
 data_int_mapped = list(map(map_to_digits, data_int))
 
-# Output
+
 print("Data Float:", data_float)
 print("Data Int:")
 for item in data_int_mapped:
